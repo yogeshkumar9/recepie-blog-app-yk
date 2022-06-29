2495dcf263d110d92a639d457bfef15e4c8df290
yogeshkumar9 <yogeshkumar_2k19mc140@dtu.ac.in>
2022-06-29 22:47:38 +0530

    2nd commit

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const app = express();
+const connectdb = require('./server/models/database');
+
 const port= process.env.port||3000;
 require('dotenv').config();
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(expresslayouts);
-
+connectdb();
 
 
 app.use(cookieParser('CookingBlogSecure'));
--- server/models/database.js
+++ server/models/database.js
@@ -2,16 +2,24 @@ const mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGODB_URI,{
 //      useNewUrlParser:true,useUnifiedTopology:true,
 // });
-const db='mongodb+srv://yogeshkumar99:sahilkumar9873@cluster0.i4f7j.mongodb.net/?retryWrites=true&w=majority'
+const URI="mongodb+srv://yogeshkumar99:sahilkumar9873@cluster0.i4f7j.mongodb.net/?retryWrites=true&w=majority";
 
 
-mongoose.connect(db).then(()=>{
-     console.log('connection successful');
-}).catch((error)=>{
-     console.log(`connection FUCK`);
-})
+// mongoose.connect(db).then(()=>{
+//      console.log('connection successful');
+// }).catch((error)=>{
+//      console.log(`connection FUCK`);
+// })
+const connectdb = async()=>{
+await mongoose.connect(URI,
+    { useunifiedTopology:true,
+     useNewUrlParser: true
+    });
+    console.log('db connected..!')
+}
 require('./Category');
 require('./recepie');
+module.exports=connectdb; 
 // db.on('error',console.error.bind(console,'connection error:'));
 // db.once('open',function(){
 //      console.log('Connected')
